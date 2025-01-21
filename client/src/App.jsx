d5eb1c630c513b222c6da64af2a589583091e4a8
EmaniAditya <esaadityareddy@gmail.com>
2025-01-21 20:59:57 +0530

    route planning done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/HomePage';
+import BlogPage from './pages/BlogPage';
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
-  return <>
-  
-  </>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/blog/:id" element={<BlogPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
