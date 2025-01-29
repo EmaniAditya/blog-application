c975a511adc0856848e2ef81f97ab11757813cb9
EmaniAditya <esaadityareddy@gmail.com>
2025-01-30 01:56:51 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/blog-application/',
   plugins: [react()],
+  // build: {
+  //   outDir: 'build', 
+  // },
 })
